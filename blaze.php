33278fd7cf5f3bc9ea17427d0e607680310afd8c
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2022-09-26 04:18:32 +0100

    updated the homepage

---
+++ blaze.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "includes/component.inc.php" ?>
+
+    <?php require_once "includes/style.inc.php" ?>
+    <title>Blaze Intro</title>
+
+    <style>
+        
+    @media (max-width:567.98px) {
+    
+        .img_small{
+            
+        }
+
+        body{
+            background: ;
+        }
+
+        #content-contain{
+            display: flex;
+            flex-direction: column;
+        }
+
+        #img_container{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+
+    }
+
+    </style>
+</head>
+
+<body>
+
+    <div  id="spinner" class="">
+
+            <div class="dot-pulse text-danger"></div>
+
+
+    </div>
+
+
+    <div class="container-fluid">
+
+
+    </div>
+
+    
+
+<?php require_once "includes/footer.inc.php" ?>
+</body>
+
+</html>
