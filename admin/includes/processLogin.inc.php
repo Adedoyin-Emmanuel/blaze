2b975393c29bd32b2035ef8abea3ec6ca5e59d9b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2022-10-11 04:42:33 +0100

    making changes to waitlist

--- admin/includes/processLogin.inc.php
+++ admin/includes/processLogin.inc.php
@@ -14,11 +14,13 @@ $login_result = $login_controller->check_credentials($userName,$password);
 
 if($login_result == 1){
 
-	echo "welcome chief, login successful";
+	
 	require_once "../../includes/session.inc.php";
 	setcookie("login", true, time() + (86400 * 30), "/");
 	$_SESSION["login"] = $_COOKIE["login"];
 
+	echo "welcome chief, login successful";
+	
 	
 
 }else{
