45e7fa0bbd2b912125f540bb2018452860b20e27
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2022-10-14 05:08:59 +0100

    i changed the code structure to suit railway.app system

--- controllers/databaseController.controller.php
+++ controllers/databaseController.controller.php
@@ -25,17 +25,17 @@
 
 				try{
 
-					// $this->serverName = "localhost";
-					// $this->userName   = "id19691524_blaze_admin";
-					// $this->password   = "Z{rdWe|zQSf9[}(c";
-					// $this->database   = "id19691524_blaze";
-					// $this->port       = 5864;
-
 					$this->serverName = "localhost";
-					$this->userName   = "root";
-					$this->password   = "";
-					$this->database   = "blaze_waitlist";
+					$this->userName   = "id19691524_blaze_admin";
+					$this->password   = "Z{rdWe|zQSf9[}(c";
+					$this->database   = "id19691524_blaze";
 					$this->port       = 5864;
+
+					// $this->serverName = "localhost";
+					// $this->userName   = "root";
+					// $this->password   = "";
+					// $this->database   = "blaze_waitlist";
+					// $this->port       = 5864;
 
 
 					$this->conn = new mysqli($this->serverName,$this->userName,$this->password, $this->database);
