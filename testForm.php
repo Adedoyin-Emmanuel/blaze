f53939f2f62eb5ff33b27d0677a4639a82a5c3a3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-13 09:03:38 +1200

    Added the navigate to home functionality to the subscribe page

--- testForm.php
+++ testForm.php
@@ -26,12 +26,26 @@
 
 <iframe width="600" height="600" src="https://bcab6ffd.sibforms.com/serve/MUIEAIa6705MbYbVmZcd7okuj_u63hW6Zxi2OGf1K7J_L8z1ZNcKiBVj9XiIHTdqRIfsh6fR2k33-i4agu7VhWTalgI7mxjR_ERK3gtmr5oT1LdC0BGBHK1Hh2L5P-6uRK8iVXmzKf1HnmlIoayVkH0gVnxJsfeKtZGDL16v165WWJL7g6KrUlvMVogGXebhCxnFgZHPFPL8TBuS" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>
 		
-	<button class="text-capitalize text-center btn btn" style="background:lightskyblue;">
+	<button class="text-capitalize text-center btn my-5" style="background:lightskyblue;" id="homeButton">
 	Home</button>
 
 
 
 </body>
+
+
+<script>
+
+	jQuery(($)=>{
+		$.noConflict();
+
+		$("#homeButton").click(()=>{
+			location.href="homePage.php";
+		})
+	})
+
+</script>
+
 </html>
 
 
